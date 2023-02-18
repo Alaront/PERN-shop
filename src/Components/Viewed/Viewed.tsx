a8bfb0c2b929415fb72206027f7abc4340a44d9b
Alexander Prikhodko <alaront.work@gmail.com>
2023-02-18 16:15:27 +0300

    viewed block

---
+++ src/Components/Viewed/Viewed.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ProductCartShort from "../Products/ProductCartShort";
+
+
+import './index.sass';
+import Bitmap from "../../images/products/Bitmap.png";
+
+const Viewed = () => {
+    return (
+        <div className={'viewed'}>
+            <p className={'viewed__title'}>Вы смотрели</p>
+            <div className={'viewed__wrapper-card'}>
+                <ProductCartShort title={'T-shirts with multiple colors, for men'} photoUrl={Bitmap} price={7.30} hrefLink={"#"} />
+                <ProductCartShort title={'T-shirts with multiple colors, for men'} photoUrl={Bitmap} price={10} oldPrice={9} hrefLink={"#"} />
+            </div>
+        </div>
+    );
+};
+
+export default Viewed;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AdSlider from "../Components/AdSlider/AdSlider";
 import Daily from "../Components/AdOffers/Daily";
 import Bestsellers from "../Components/Bestsellers/Bestsellers";
+import Viewed from "../Components/Viewed/Viewed";
 
 const Main = () => {
     return (
@@ -9,6 +10,7 @@ const Main = () => {
             <AdSlider />
             <Daily />
             <Bestsellers />
+            <Viewed />
         </div>
     );
 };
