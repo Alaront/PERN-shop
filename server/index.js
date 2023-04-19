0b02446abd5a0e564c2cc6e7b13ad716f1b5a002
Alexander Prikhodko <alaront.work@gmail.com>
2023-04-19 11:53:51 +0300

    BACK: init sequelize

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {config} from "dotenv";
-
+import sequelize from './db.js'
 config()
 const app = new express();
 const PORT = process.env.PORT || 5000;
@@ -9,6 +9,17 @@ app.get('/', (req, res) => {
     res.json('work')
 })
 
-app.listen(PORT, () => {
-    console.log('Server started ', PORT)
-})
+const startServer = async () => {
+  try {
+      await sequelize.authenticate();
+      await sequelize.sync();
+
+      app.listen(PORT, () => {
+          console.log('Server started ', PORT)
+      })
+  }  catch (e) {
+      console.log(e)
+  }
+}
+
+startServer()
