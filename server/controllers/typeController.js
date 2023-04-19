9c2c957f2e42620345f929d129af7d863d8eec89
Alexander Prikhodko <alaront.work@gmail.com>
2023-04-19 17:45:16 +0300

    BACK: type CRUD

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,9 +1,81 @@
-class TypeController {
-    async create(res, req) {
+import { transliterate as tr } from 'transliteration';
+import ApiError from "../Utils/ApiError.js";
+import {Type} from "../models/models.js";
+import {where} from "sequelize";
 
+class TypeController {
+    async create(req, res, next) {
+        try {
+            let {name, slug} = req.body;
+
+            if(!slug) {
+                slug = tr(name).replace(' ', '') + '_' + Date.now()
+            }
+
+            const type = await Type.create({name, slug})
+
+            return res.json(type)
+
+        } catch (e) {
+            return next(ApiError.internal('Error', e))
+        }
     }
 
-    async get(req, res) {
+    async get(req, res, next) {
+        try {
+            const type = await Type.findAll()
+
+            return res.json(type)
+
+        } catch (e) {
+            return next(ApiError.internal('Error', e))
+        }
+    }
+
+    async update(req, res, next) {
+        try {
+            const {id, name, slug} = req.body;
+
+            if(!id) {
+                return next(ApiError.badRequest('Not set id'))
+            }
+
+            let type = null;
+
+            if(name && !slug) {
+                type = await Type.update({name: name}, {where: {id: id}, returning: true})
+            }
+
+            if(!name && slug) {
+                type = await Type.update({slug: slug}, {where: {id: id}, returning: true})
+            }
+
+            if(name && slug) {
+                type = await Type.update({name: name, slug: slug}, {where: {id: id}, returning: true})
+            }
+
+
+            return res.json(type)
+        } catch (e) {
+            return next(ApiError.internal('Error', e))
+        }
+    }
+
+    async del(req, res, next) {
+        try {
+            const {id} = req.body;
+
+            if(!id) {
+                return next(ApiError.badRequest('Not set id'))
+            }
+
+            await Type.destroy({where: {id}})
+
+            return res.json({message: 'success'})
+
+        }catch (e) {
+            return next(ApiError.internal('Error', e))
+        }
 
     }
 }
--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -4,6 +4,8 @@ import typeController from "../controllers/typeController.js";
 const router = new Router();
 
 router.post('', typeController.create)
+router.patch('', typeController.update)
+router.delete('', typeController.del)
 router.get('', typeController.get)
 
 export default router
