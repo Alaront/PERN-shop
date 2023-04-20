68f328fad0aa29f641235be3e894f6779763a7f7
Alexander Prikhodko <alaront.work@gmail.com>
2023-04-21 00:11:52 +0300

    FRONT: 404 page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import './../styles/notFound.sass'
+import {NavLink} from "react-router-dom";
+
 const NotFound = () => {
     return (
-        <div>
-            404
+        <div className={'content not-found-page'}>
+            <h6>Упс.. Эта страница не найдена</h6>
+            <p>Вы всегда можете <NavLink to={'/'}>вернуться на главную</NavLink></p>
+
+            <span>404</span>
         </div>
     );
 };
