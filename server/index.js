21a3ed20fccc862aa66f9ee46fa31539819628f9
Alexander Prikhodko <alaront.work@gmail.com>
2023-05-10 17:31:51 +0300

    BACK: fix get img url

--- server/index.js
+++ server/index.js
@@ -31,7 +31,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 app.use(fileUpload())
-app.use(express.static(path.resolve(__dirname, 'static')))
+//app.use(express.static(path.resolve(__dirname, 'static')))
+app.use('/img', express.static('static/img'))
+
 app.get('/', (req, res) => {
     return res.json('this is main page')
 })
